19ddad8530e2c6b3b0c8679657eb4cb8450c6b34
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-12-19 09:20:42 -0500

    Add methods defined by user

--- menus.py
+++ menus.py
@@ -1,3 +1,5 @@
+import os
+
 menu = "1. Registrar Alumno\n2. Registrar Notas\n3. Buscar estudiante\n4. Salir"
 subMenuNotas = "1.Parciales\n2.Quices\n3.Trabajos\n4.Regresar al menu principal"
 hasError = True
@@ -7,7 +9,24 @@ def menuPrincipal() -> int:
     print(menu)
     while (hasError == True):
         try:
-            hasError = False
+            #hasError = False
             return int(input(":)"))
         except ValueError:
             hasError = True
+
+def menuNotas() -> int:
+    os.system('cls')
+    header = """
+    ********************************
+    *   MENU REGISTRO DE NOTAS     *
+    ********************************
+    """
+    global hasError
+    print(header)
+    print(subMenuNotas)
+    while (hasError == True):
+        try:
+            return int(input(":)"))
+        except ValueError:
+            hasError = True
+
